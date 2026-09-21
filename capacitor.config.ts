7a4b008cec26903aed6e895a4425a25529b6494b
toma satou <84651853+sato-toma@users.noreply.github.com>
2026-09-21 15:09:53 +0900

    Add Android APK CI

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+    appId: "com.cadyomi.inspector",
+    appName: "CADYomi",
+    webDir: "dist",
+};
+
+export default config;
